4ac1c804640f74ee50b50cadccfa7d354a31128a
tanmoytonu <tanmoyacharjee18@gmail.com>
2020-04-23 00:43:49 +0600

    done

---
+++ src/firebase.Config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwTBAJPnZ0dZpmTcbg4S58cA_PU4XGVZg",
+    authDomain: "emajohn-simple-a8c19.firebaseapp.com",
+    databaseURL: "https://emajohn-simple-a8c19.firebaseio.com",
+    projectId: "emajohn-simple-a8c19",
+    storageBucket: "emajohn-simple-a8c19.appspot.com",
+    messagingSenderId: "377667138389",
+    appId: "1:377667138389:web:906c2944660125b27e35dc",
+    measurementId: "G-SGJKMPNR49"
+  };
+  export default firebaseConfig;
